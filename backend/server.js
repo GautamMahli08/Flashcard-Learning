fb8dd4770abd596d29545aa82c03aa3ff9694bee
Gautam Mahli <140971352+GautamMahli08@users.noreply.github.com>
2025-02-21 04:42:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://learnwithflashcard.netlify.app/", // Replace with your frontend URL
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // MongoDB Connection
